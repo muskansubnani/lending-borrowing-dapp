8a5c21d3891be82daa0e5ee87f03764234e5035f
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-10-16 23:56:06 +0300

    more work, added chakra, router

---
+++ frontend/src/containers/navigation/routes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+export const Routes = () => {
+    <Routes>
+        <Route path="/account/signup" element={<></>}/>
+        <Route path="users" element={<></>} />
+    </Routes>
+}
