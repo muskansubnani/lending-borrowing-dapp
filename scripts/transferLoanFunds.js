db6630c01c7461a48b1ae7df7d0affebd8780217
Muskan <muskansubnani98@gmail.com>
2022-10-28 16:14:00 -0500

    update scripts based on nft collateral logic

--- scripts/transferLoanFunds.js
+++ scripts/transferLoanFunds.js
@@ -2,8 +2,7 @@ const LendBorrow= artifacts.require("LendBorrowContract");
 const main = async (cb) => {
     try {
         const lendBorrow = await LendBorrow.deployed();
-        let user = (await web3.eth.getAccounts())[2];
-        let txn = await lendBorrow.transferLoanFunds(0, {from: user});
+        let txn = await lendBorrow.transferLoanFunds(0, {from: ""});
         console.log(await web3.eth.getBalance(lendBorrow.address));
         console.log(txn);
         console.log(txn.logs[0].args);
