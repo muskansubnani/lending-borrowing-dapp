c46b4079817b81a32797345787e38f20455028b1
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-11-01 23:55:24 +0200

    use async await for fetching collections

--- frontend/src/data/hooks/useCollectionsForOwner.js
+++ frontend/src/data/hooks/useCollectionsForOwner.js
@@ -20,13 +20,18 @@ export const useNftCollectionsForOwner = () => {
 
     const options = { method: "GET" };
 
-    fetch(
-      `https://testnets-api.opensea.io/api/v1/collections?asset_owner=${address}&offset=0&limit=300`,
-      options
-    )
-      .then((response) => response.json())
-      .then((response) => setOwnerNftCollections(response))
-      .catch((err) => console.error(err));
+    const getCollections = async () => {
+      const response = await fetch(
+        `https://testnets-api.opensea.io/api/v1/collections?asset_owner=${address}&offset=0&limit=300`,
+        options
+      );
+      
+      const collections = await response.json();
+
+      setOwnerNftCollections(collections);
+    };
+
+    getCollections();
   }, []);
 
   return { ownerNftCollections };
