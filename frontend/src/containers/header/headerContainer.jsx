5dad692096dc01896d38f4bb142c64cac7816196
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-11-02 20:13:45 +0200

    move contractWalletType in its own hook

--- frontend/src/containers/header/headerContainer.jsx
+++ frontend/src/containers/header/headerContainer.jsx
@@ -2,48 +2,33 @@ import { useNavigate } from "react-router";
 import { useAccount } from "wagmi";
 import { useWallet } from "./../../data/context/walletContext";
 import { useEffect } from "react";
-import abi from "./../../contracts/LendBorrower.json";
-import { ethers } from "ethers";
+import { useContractWalletType } from './../../data/hooks/useContractWalletType';
 
 export const HeaderContainer = () => {
   const navigation = useNavigate();
   const { address } = useAccount();
-  const { walletType, setWalletType } = useWallet();
-
-  const contractAbi = abi.abi;
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const lenderborrowerContract = new ethers.Contract(
-    "0xB358B0851Feb9853Cf2D016E5c2653A847659098",
-    contractAbi,
-    provider
-  );
+  const { setWalletType } = useWallet();
+  const {contractWalletType} = useContractWalletType();
 
   useEffect(() => {
     if (!address) {
       return;
     }
-
-    const getAccountType = async () => {
-      const accountType = await lenderborrowerContract.getAccountType(address);
-
-      setWalletType(accountType);
-    };
-
-    getAccountType().catch(console.error);
+    setWalletType(contractWalletType);
   }, [address]);
 
   useEffect(() => {
-    if(!walletType)
+    if(!contractWalletType)
       return;
       
-    if (walletType === "InActive") {
+    if (contractWalletType === "InActive") {
       navigation("/signup");
       return;
     }
 
     navigation(`/`);
     return;
-  }, [walletType]);
+  }, [contractWalletType]);
 
   return <div></div>;
 };
---
+++ frontend/src/data/hooks/useContractWalletType.js
@@ -0,0 +1,31 @@
+import abi from "../../contracts/LendBorrower.json";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+
+export const useContractWalletType = () => {
+  const { address } = useAccount();
+  const [contractWalletType, setContractWalletType] = useState(null);
+
+  //this should come from context, so smth like const {lenderBorrowerContract} = useLenderContract();
+  const contractAbi = abi.abi;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const lenderborrowerContract = new ethers.Contract(
+    "0xB358B0851Feb9853Cf2D016E5c2653A847659098",
+    contractAbi,
+    provider
+  );
+
+  useEffect(() => {
+    const getAccountType = async () => {
+      const accountType = await lenderborrowerContract.getAccountType(address);
+
+      setContractWalletType(accountType);
+      console.log(accountType, 'accType');
+    };
+
+    getAccountType().catch(console.error);
+  }, []);
+
+  return { contractWalletType };
+};
