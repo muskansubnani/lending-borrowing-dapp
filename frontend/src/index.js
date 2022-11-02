55b10f9a34d2885e1161af6111d4fbe641ea0914
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-11-02 16:34:44 +0200

    potential fix for calling etherscan like crazy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,26 +5,25 @@ import App from "./App";
 import { createClient, WagmiConfig, chain, configureChains } from "wagmi";
 import { ChakraProvider } from "@chakra-ui/react";
 import { infuraProvider } from "wagmi/providers/infura";
-import { publicProvider } from "wagmi/providers/public";
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
-import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 window.Buffer = require("buffer/").Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const { chains, provider } = configureChains(
-  [chain.goerli, chain.localhost],
+  [chain.goerli],
   [
     infuraProvider({
       apiKey: process.env.REACT_APP_INFURA_API_KEY,
       priority: 0,
     }),
     alchemyProvider({
-      priority:1
+      apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+      priority: 1,
     }),
-    publicProvider({ priority:2 }),
   ]
 );
 
@@ -40,11 +39,11 @@ const client = createClient({
 });
 
 root.render(
-    <ChakraProvider>
-      <WagmiConfig client={client}>
-        <RainbowKitProvider chains={chains}>
-          <App />
-        </RainbowKitProvider>
-      </WagmiConfig>
-    </ChakraProvider>
+  <ChakraProvider>
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chains}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </ChakraProvider>
 );
