f7e57290c38b6984afb4ffffaeb5b1e0ad4493fd
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-11-06 22:00:50 +0200

    dashboard link items updated based on existing account / new account

--- frontend/src/data/hooks/useLinkItems.js
+++ frontend/src/data/hooks/useLinkItems.js
@@ -1,13 +1,19 @@
 import { useWallet } from "./../../data/context/walletContext";
 import { FiHome, FiTrendingUp, FiCompass } from "react-icons/fi";
 import { useEffect, useState } from "react";
+import { useContractWalletType } from "./contract/useContractWalletType";
 
 export const useLinkItems = () => {
   const { walletType } = useWallet();
+  const { contractWalletType } = useContractWalletType();
+
   const [linkItems, setLinkItems] = useState([]);
 
   useEffect(() => {
-    const items = getLinkItems(walletType);
+    if(!walletType)
+      return;
+      
+    const items = getLinkItems(walletType, contractWalletType);
 
     setLinkItems(items);
   }, [walletType]);
@@ -15,10 +21,36 @@ export const useLinkItems = () => {
   return { linkItems };
 };
 
-const getLinkItems = (walletType) => {
+const getLinkItems = (walletType, contractWalletType) => {
+  return contractWalletType === "InActive"
+    ? getLinkItemsForNewCustomer(walletType)
+    : getLinkItemsForExistingCustomer(walletType);
+};
+
+const getLinkItemsForNewCustomer = (walletType) => {
   if (walletType === "borrower") {
     return [
       { name: "New Loan", icon: FiTrendingUp, path: "createLoan" },
+      { name: "FAQ", icon: FiCompass, path: "faq" },
+    ];
+  }
+
+  if (walletType === "lender") {
+    return [
+      { name: "New Lending", icon: FiTrendingUp, path: "createlending" },
+      { name: "FAQ", icon: FiCompass, path: "faq" },
+    ];
+  }
+
+  return [
+    { name: "Home", icon: FiHome, path: "signup" },
+    { name: "FAQ", icon: FiCompass, path: "faq" },
+  ];
+};
+
+const getLinkItemsForExistingCustomer = (walletType) => {
+  if (walletType === "borrower") {
+    return [
       { name: "Account History", icon: FiTrendingUp, path: "history" },
       { name: "Transactions", icon: FiCompass, path: "transactions" },
       { name: "FAQ", icon: FiCompass, path: "faq" },
@@ -27,7 +59,6 @@ const getLinkItems = (walletType) => {
 
   if (walletType === "lender") {
     return [
-      { name: "New Lending", icon: FiTrendingUp, path: "createlending" },
       { name: "Account History", icon: FiTrendingUp, path: "history" },
       { name: "Transactions", icon: FiCompass, path: "transactions" },
       { name: "FAQ", icon: FiCompass, path: "faq" },
