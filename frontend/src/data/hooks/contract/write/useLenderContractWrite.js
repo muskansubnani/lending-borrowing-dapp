8280a95774fea8265636f6a44d2dac06dd894194
Bogdan Paraschiv <bogdan.paraschiv@inspire-x.eu>
2022-11-14 00:16:54 +0200

    add toast to useLenderContractWrite hook(maybe move to another component)

--- frontend/src/data/hooks/contract/write/useLenderContractWrite.js
+++ frontend/src/data/hooks/contract/write/useLenderContractWrite.js
@@ -1,26 +1,76 @@
 import { ethers } from "ethers";
 import { useLenderBorrowerContract } from "../../../context/lenderBorrowerContractContext";
+import { useToast } from "@chakra-ui/react";
 
 export const useLenderContractWrite = () => {
   const { lenderBorrowerContract } = useLenderBorrowerContract();
+  const toast = useToast();
 
   const createLender = async (amount, duration) => {
-    const lenderTx = await lenderBorrowerContract.createLender(duration, {
-      value: ethers.utils.parseEther(amount.toString()),
-    });
+    try {
+      const lenderTx = await lenderBorrowerContract.createLender(duration, {
+        value: ethers.utils.parseEther(amount.toString()),
+      });
 
-    const receipt = await lenderTx.wait();
+      const receipt = await lenderTx.wait();
 
-    return receipt;
+      toast({
+        title: "Transaction succeded",
+        position: "bottom-right",
+        status: "success",
+        isClosable: true,
+      });
+
+      return receipt;
+    } catch (exception) {
+      toast({
+        title: "Transaction failed",
+        position: "bottom-right",
+        status: "error",
+        isClosable: true,
+      });
+    }
   };
 
   const redeemInterest = async (lenderId) => {
-    await lenderBorrowerContract.redeemLendersInterest(lenderId);
+    try {
+      await lenderBorrowerContract.redeemLendersInterest(lenderId);
+
+      toast({
+        title: "Transaction succeded",
+        position: "bottom-right",
+        status: "success",
+        isClosable: true,
+      });
+    } catch (exception) {
+      toast({
+        title: "Transaction failed",
+        position: "bottom-right",
+        status: "error",
+        isClosable: true,
+      });
+    }
   };
 
   const retrieveFunds = async (lenderId) => {
-    await lenderBorrowerContract.retrieveLendersFund(lenderId);
+    try {
+      await lenderBorrowerContract.retrieveLendersFund(lenderId);
+
+      toast({
+        title: "Transaction succeded",
+        position: "bottom-right",
+        status: "success",
+        isClosable: true,
+      });
+    } catch (exception) {
+      toast({
+        title: "Transaction failed",
+        position: "bottom-right",
+        status: "error",
+        isClosable: true,
+      });
+    }
   };
 
-  return {createLender, redeemInterest, retrieveFunds};
+  return { createLender, redeemInterest, retrieveFunds };
 };
